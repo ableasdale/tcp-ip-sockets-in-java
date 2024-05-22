8d88c79e799684daa539d8f4f6c57cbbcfb92514
Alex Bleasdale <ableasdale@confluent.io>
2024-05-22 17:38:26 +0100

    Everything so far..

---
+++ src/main/java/UDPEchoClientTimeout.java
@@ -0,0 +1,10 @@
+public class UDPEchoClientTimeout {
+
+    private static final int TIMEOUT_MS = 3000;
+    private static final int MAX_TRIES = 5;
+
+
+    public static void main(String[] args) {
+
+    }
+}
